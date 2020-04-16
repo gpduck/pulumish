afd1b1d37a3e1b9de89a693715b700dc967958a5
Chris Duck <chris.duck@vizientinc.com>
2020-04-16 18:02:23 -0500

    Add force to allow deleting stacks with resources still in them.

--- src/ExportedFunctions/Remove-PulumiStack.ps1
+++ src/ExportedFunctions/Remove-PulumiStack.ps1
@@ -4,6 +4,8 @@ function Remove-PulumiStack {
         [Parameter(ValueFromPipeline=$true)]
         $Stack,
 
+        [switch]$Force,
+
         $Pulumi = $Global:DefaultPulumi
     )
     process {
@@ -13,6 +15,9 @@ function Remove-PulumiStack {
         }
         $url = "/api/stacks/$($Pulumi.Org)/$ProjectName/$StackName"
         if($PSCmdlet.ShouldProcess("$($Pulumi.Org)/$ProjectName/$StackName", "Delete Stack")) {
+            if($Force) {
+                $Url = "${Url}?force=true"
+            }
             Invoke-PulumiApi -Method DELETE -Url $Url -Pulumi $Pulumi
         }
     }
